46d4b878cb9fea9d9e3bcba959553dea394c3944
vickiliang <62454045+vickiliang@users.noreply.github.com>
2020-04-28 20:38:51 +1000

    add in random links

---
+++ js/randomeat.js
@@ -0,0 +1,13 @@
+
+
+var randomlinks=new Array()
+
+randomlinks[0]="recipes/chickpea cookies.html"
+randomlinks[1]="https://www.instagram.com/home____cooked/"
+randomlinks[2]="https://www.themodernhouse.com/"
+randomlinks[3]="https://bread-on.earth/"
+randomlinks[4]="https://greenkitchenstories.com/potato-pizza/"
+
+function randomlink(){
+window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
+}
